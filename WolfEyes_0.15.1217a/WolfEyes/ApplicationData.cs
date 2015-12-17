039b75595b920fb23be2a872130d5ccadea2b5c8
Rippax <dj.jake7@gmail.com>
2015-12-17 09:02:05 +0100

    WolfEyes 0.15.1216a
    Fix :
    |-OpenCV and Emgu rollback
    |-Finger detection
    
    Add :
    |-Left click is now supported
    
    Mod :
    |-Detection by reference now use contours and hull to detect the hand

---
+++ WolfEyes_0.15.1217a/WolfEyes/ApplicationData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Windows.Forms;
+
+namespace WolfEyes
+{
+    class ApplicationData
+    {
+        public static Wizard wizard;
+        public static Dictionary<string, Preset> presets = new Dictionary<string, Preset>();
+        public static Preset selectedPreset = null;
+        public static string workingDir = "";
+        public static int fpsCount = 0;
+
+        public static void LoadPreset(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                string fileContent = File.ReadAllText(fileName);
+                selectedPreset = (Preset)JsonConvert.DeserializeObject(fileContent, typeof(Preset), new StringEnumConverter());
+                selectedPreset.FileName = fileName;
+            }
+            else
+            {
+                MessageBox.Show("The selected preset does not exist !", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static void SavePreset(string fileName)
+        {
+            string fileContent = JsonConvert.SerializeObject(presets[selectedPreset.Name]);
+            File.WriteAllText(fileName, fileContent);
+            selectedPreset.FileName = fileName;
+        }
+    }
+}
