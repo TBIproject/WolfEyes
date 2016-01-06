6045ac32c7fac063a9d52aabf6ff1a9a66e82fe4
Rippax <dj.jake7@gmail.com>
2016-01-06 17:54:18 +0100

    WolfEyes 0.16.0104a
    YOLO UPDATE !!!

---
+++ WolfEyes_0.16.0104a/WolfEyes/D2Point.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WolfEyes
+{
+    public class D2Point
+    {
+        public double x;
+        public double y;
+
+        public D2Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static D2Point operator +(D2Point a, D2Point b)
+        {
+            return new D2Point(a.x + b.x, a.y + b.y);
+        }
+
+        public static D2Point operator -(D2Point a, D2Point b)
+        {
+            return new D2Point(a.x - b.x, a.y - b.y);
+        }
+
+        public static D2Point operator -(D2Point a, double b)
+        {
+            return a - new D2Point(b, b);
+        }
+
+        public static D2Point operator /(D2Point a, D2Point b)
+        {
+            return new D2Point(a.x / b.x, a.y / b.y);
+        }
+
+        public static D2Point operator /(D2Point a, double size)
+        {
+            return a / (new D2Point(size, size));
+        }
+
+        public static D2Point operator *(D2Point a, D2Point b)
+        {
+            return new D2Point(a.x * b.x, a.y * b.y);
+        }
+
+        public static D2Point operator *(D2Point a, double size)
+        {
+            return a * (new D2Point(size, size));
+        }
+
+        public static D2Point operator %(D2Point a, D2Point b)
+        {
+            return new D2Point((a.x + b.x) / 2, (a.y + b.y) / 2);
+        }
+
+        public static double Abs(D2Point a)
+        {
+            return Math.Sqrt(a.x * a.x + a.y * a.y);
+        }
+
+        public double Length
+        {
+            get
+            {
+                return Math.Sqrt(this.x * this.x + this.y * this.y);
+            }
+        }
+
+        public D2Point Unit()
+        {
+            return this / this.Length;
+        }
+
+        public override string ToString()
+        {
+            return "X : " + x + " | Y : " + y;
+        }
+    }
+}
---
+++ WolfEyes_0.16.0104a/WolfEyes/FrmDeviceSelection.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace WolfEyes
+{
+    partial class FrmDeviceSelection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picDevice1 = new System.Windows.Forms.PictureBox();
+            this.picDevice2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudDevice1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nudDevice2 = new System.Windows.Forms.NumericUpDown();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picDevice1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDevice2)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDevice1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDevice2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picDevice1
+            // 
+            this.picDevice1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picDevice1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picDevice1.Location = new System.Drawing.Point(12, 76);
+            this.picDevice1.Name = "picDevice1";
+            this.picDevice1.Size = new System.Drawing.Size(304, 171);
+            this.picDevice1.TabIndex = 0;
+            this.picDevice1.TabStop = false;
+            // 
+            // picDevice2
+            // 
+            this.picDevice2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picDevice2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picDevice2.Location = new System.Drawing.Point(331, 76);
+            this.picDevice2.Name = "picDevice2";
+            this.picDevice2.Size = new System.Drawing.Size(304, 171);
+            this.picDevice2.TabIndex = 0;
+            this.picDevice2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(647, 70);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 30);
+            this.label2.Margin = new System.Windows.Forms.Padding(3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Type the IDs of your plugged devices.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Please select capture devices";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(560, 351);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 0;
+            this.btnNext.Text = "Next >";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(398, 351);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 253);
+            this.label3.Margin = new System.Windows.Forms.Padding(3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Device ID :";
+            // 
+            // nudDevice1
+            // 
+            this.nudDevice1.Location = new System.Drawing.Point(12, 272);
+            this.nudDevice1.Name = "nudDevice1";
+            this.nudDevice1.Size = new System.Drawing.Size(304, 20);
+            this.nudDevice1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(328, 253);
+            this.label4.Margin = new System.Windows.Forms.Padding(3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Device ID :";
+            // 
+            // nudDevice2
+            // 
+            this.nudDevice2.Location = new System.Drawing.Point(331, 272);
+            this.nudDevice2.Name = "nudDevice2";
+            this.nudDevice2.Size = new System.Drawing.Size(304, 20);
+            this.nudDevice2.TabIndex = 4;
+            this.nudDevice2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(286, 310);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(479, 351);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevious.TabIndex = 1;
+            this.btnPrevious.Text = "< Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // FrmDeviceSelection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 386);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.nudDevice2);
+            this.Controls.Add(this.nudDevice1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.picDevice2);
+            this.Controls.Add(this.picDevice1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmDeviceSelection";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Capture device selection - Preset Wizard";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmSelectCamera_FormClosing);
+            this.Load += new System.EventHandler(this.FrmSelectCamera_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picDevice1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDevice2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDevice1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDevice2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picDevice1;
+        private System.Windows.Forms.PictureBox picDevice2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudDevice1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown nudDevice2;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnPrevious;
+    }
+}
