e15061351c85f58120fd1f04c04df347661d5f90
Rippax <dj.jake7@gmail.com>
2016-03-03 17:04:46 +0100

    latest shitty commit

--- st_test_dynalosaure.py
+++ st_test_dynalosaure.py
@@ -12,15 +12,17 @@ cam.setFOV(horizontal=math.radians(92.0))
 yolol = 20
 				  
 cam.getFrame()
-ref = cam.frame
-maxes = cam.frame
-mines = cam.frame
+frame = cf.Gamma(cam.frame, 2)
+ref = frame
+maxes = frame
+mines = frame
 
 for i in range(9):
 	cam.getFrame()
-	ref = ref / 2 + cam.frame / 2
-	maxes = np.maximum(maxes, cam.frame)
-	mines = np.minimum(mines, cam.frame)
+	frame = cf.Gamma(cam.frame, 2)
+	ref = ref / 2 + frame / 2
+	maxes = np.maximum(maxes, frame)
+	mines = np.minimum(mines, frame)
 	
 tolerance_maxes = (maxes.astype(np.int32) + yolol).clip(0, 255).astype(np.uint8)
 tolerance_mines = (mines.astype(np.int32) - yolol).clip(0, 255).astype(np.uint8)
@@ -30,17 +32,18 @@ print 'looping...'
 
 while 1:
 	cam.getFrame()
-	valid_pixels_mask = np.logical_and(cam.frame > mines, cam.frame < maxes) * 255
-	over_pixels_mask = np.logical_and(cam.frame >= maxes, cam.frame <= tolerance_maxes) * 255
-	under_pixels_mask = np.logical_and(cam.frame >= tolerance_mines, cam.frame <= mines) * 255
-	unknown_over_mask = (cam.frame > tolerance_maxes) * 255
-	unknown_under_mask = (cam.frame < tolerance_mines) * 255
-	corrected_frame = (cam.frame & valid_pixels_mask) + (maxes & over_pixels_mask) + (mines & under_pixels_mask) + (cam.frame & unknown_over_mask) + (cam.frame & unknown_under_mask)
+	frame = cf.Gamma(cam.frame, 2)
+	valid_pixels_mask = np.logical_and(frame > mines, frame < maxes) * 255
+	over_pixels_mask = np.logical_and(frame >= maxes, frame <= tolerance_maxes) * 255
+	under_pixels_mask = np.logical_and(frame >= tolerance_mines, frame <= mines) * 255
+	unknown_over_mask = (frame > tolerance_maxes) * 255
+	unknown_under_mask = (frame < tolerance_mines) * 255
+	corrected_frame = (frame & valid_pixels_mask) + (maxes & over_pixels_mask) + (mines & under_pixels_mask) + (frame & unknown_over_mask) + (frame & unknown_under_mask)
 	
 	diff = cv2.absdiff(corrected_frame.astype(np.uint8), ref)
 	result = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
 	result = (result >= 20) * 255
-	result = cv2.medianBlur(result.astype(np.uint8), 5)
+	result = cv2.medianBlur(result.astype(np.uint8), 3)
 	
 	temoinosaure = cv2.absdiff(cam.frame, ref)
 	temoinosaure = cv2.cvtColor(temoinosaure, cv2.COLOR_BGR2GRAY)
@@ -48,11 +51,11 @@ while 1:
 	#temoinosaure = cv2.medianBlur(temoinosaure.astype(np.uint8), 5)
 	
 	# Affichage
-	cv2.imshow('cam', cam.frame)
+	cv2.imshow('cam', frame)
 	#cv2.imshow('diff', diff.astype(np.uint8))
 	cv2.imshow('corrected_frame', corrected_frame.astype(np.uint8))
 	cv2.imshow('result', result.astype(np.uint8))
-	cv2.imshow('temoinosaure', temoinosaure.astype(np.uint8))
+	#cv2.imshow('temoinosaure', temoinosaure.astype(np.uint8))
 	
 	# Input management
 	sKey = Camera.waitKey()
