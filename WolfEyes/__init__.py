abde9b7dfdabdc57a5d2ed3254f96b0fa9820ac8
WKnight02 <wknight02@gmail.com>
2016-11-04 17:09:04 +0100

    from WolfEyes import *

---
+++ WolfEyes/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from .Camera import *
+from .ProcessUnit import *
+from .WolfEye import *
--- WolfEyes/wip__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import cv2
-from . import tbiTools
-from . import MouseControl
-from . import CustomFilter
-from Camera import *
-from D2Point import D2Point
-from pyon import pyon
