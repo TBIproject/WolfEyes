1d092802cef9f6591707206915ad0c85a4dd5bd8
WKnight02 <wknight02@gmail.com>
2016-11-17 02:24:40 -0500

    ProcessUnit stack rework

--- WolfEyes/Project/ProcessUnit.py
+++ WolfEyes/Project/ProcessUnit.py
@@ -11,6 +11,40 @@ from ..Utils import TypeChecker
 
 class ImageProcessingException(Exception): pass
 
+# The function enrichissment object
+class Process(object):
+
+    def __init__(this, function, *args, **kargs):
+        if not callable(function):
+            raise TypeError("First argument should be callable, got %s" % type(function))
+
+        this.owner = None
+        this.function = function
+        this.setArgs(*args, **kargs)
+
+    def setOwner(this, owner):
+
+        this.owner = owner
+
+    def __call__(this, frame, *args, **kargs):
+
+        owner = this.owner
+        pargs = args or this.args
+        pkargs = kargs or this.kargs
+
+        if owner is None:
+            return this.function(frame, *pargs, **pkargs)
+        else:
+            return this.function(owner, frame, *pargs, **pkargs)
+
+    def setArgs(this, *args, **kargs):
+
+        this.kargs = kargs
+        this.args = args
+
+    def __repr__(this): return "<Process over %s>" % this.function
+
+# The webcam based object
 class ProcessUnit(Camera):
 
     def __init__(this, id = None, **kargs):
@@ -49,14 +83,16 @@ class ProcessUnit(Camera):
             this._PROCESSED_FRAME = source
 
             for process in this._PROCESS_STACK:
-                result = process.function(current, *process.args, **process.kargs)
+                result = process(current)
+
                 if result is not None: current = result
                 this._PROCESSED_FRAME = current
 
                 if pyon:
-                    pyonReturn = process.copy()
-                    pyonReturn.update({'frame': current})
-                    yield pyonReturn
+                    yield pyon(
+                        process = process,
+                        frame = current
+                    )
                 else:
                     yield current
 
@@ -74,29 +110,38 @@ class ProcessUnit(Camera):
         return this._PROCESSED_FRAME
 
     # Decorator for adding processes to the stack
-    @TypeChecker.args(index = int, replace = bool)
-    def addProcess(this, func = None, **kargs):
+    @TypeChecker.args(index = int, replace = bool, own = bool)
+    def addProcess(this, function = None, **kargs):
 
         index = kargs.get('index', len(this._PROCESS_STACK))
         replace = kargs.get('replace', False)
+        own = kargs.get('own', False)
 
-        def add(func):
+        def add(function, owner = None):
 
-            if not callable(func):
-                raise TypeError("func should be callable, got %s" % type(func))
+            if not callable(function):
+                raise TypeError("Function should be callable, got %s" % type(function))
 
-            process = pyon(
-                function = func,
-                args = kargs.get('args', ()),
-                kargs = kargs.get('kargs', {})
+            # Create the rich function
+            process = Process(function,
+                *kargs.get('args', ()),
+                **kargs.get('kargs', {})
             )
+
+            # Pass the reference to current cam
+            if own: process.setOwner(this)
 
             if replace:
                 this._PROCESS_STACK.pop(index)
             this._PROCESS_STACK.insert(index, process)
 
             if kargs.get('verbose', False):
-                print('added process:', func)
-            return func
+                print('added process:', function)
+            return function
 
-        return add(func) if func else add
+        return add(function) if function else add
+
+    # Decorator for adding processes to the stack with owner awareness
+    def addAwareProcess(this, function = None, **kargs):
+        kargs['own'] = True
+        return this.addProcess(function, **kargs)
